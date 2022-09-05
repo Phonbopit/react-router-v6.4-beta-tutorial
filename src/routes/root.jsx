349781b4d6f51ac6cfa884903836f79e2411d11e
Chai Phonbopit <phonbopit@gmail.com>
2022-09-05 20:47:56 +0700

    Finish tutorial

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,36 +1,101 @@
+import { useEffect } from 'react'
+import {
+  Outlet,
+  useNavigation,
+  useLoaderData,
+  Form,
+  NavLink,
+  useSubmit,
+} from 'react-router-dom'
+import { createContact, getContacts } from '../contacts'
+
+export async function loader({ request }) {
+  const url = new URL(request.url)
+  const q = url.searchParams.get('q')
+  const contacts = await getContacts(q)
+  return { contacts, q }
+}
+
+export async function action() {
+  await createContact()
+}
+
 export default function Root() {
+  const { contacts, q } = useLoaderData()
+  const navigation = useNavigation()
+  const submit = useSubmit()
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q')
+
+  useEffect(() => {
+    document.getElementById('q').value = q
+  }, [q])
+
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
+              onChange={(event) => {
+                const isFirstSearch = q == null
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch,
+                })
+              }}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <form method="post">
+          </Form>
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <a href={`contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{' '}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
-      <div id="detail"></div>
+      <div
+        id="detail"
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
+        <Outlet />
+      </div>
     </>
   )
 }
